0575c8bd6222a1f05c76dbdb0094998499dd6a8f
Jesus Graterol <grate94jesus@gmail.com>
2025-07-04 15:30:34 -0400

    Navbar: empezando a implementar las variables y los atributos, primera version

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,5 @@
 export const createNavbar = () => {
   const navbarContainer = document.createElement("navbar");
   const logo = document.createElement("div");
-  logo.innerHTML = '<img src="/img/LOGO AMESA NUEVO.png" alt="Logo-AMESA">';
-  navbarContainer.appendChild(logo);
+  const img = document.createElement("img");
 };
